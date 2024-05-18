fc3eb64952aa5158b6f54eed17595a82ab82eaaa
Gean Almeida <gomesgean81@gmail.com>
2024-05-18 13:53:49 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function RelogioDigital() {
+  const[hora, setHora] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() =>{
+    const interval = setInterval(() =>{
+      setHora(new Date().toLocaleTimeString());
+    },1000);
+
+    return() => clearInterval(interval);
+  },[]);
+  
+
+  return(
+    <div>
+      <h2>
+        Relógio Digital
+      </h2>
+      <p>{hora}</p>
+    </div>
+  );
+}
+
+
+
+export default RelogioDigital
